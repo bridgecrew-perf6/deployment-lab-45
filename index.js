f25aacf1e7961f7db8d39d7f98c9d9da66db3241
Mike Davis <mike.davis@psyvad.com>
2022-02-24 15:38:56 -0700

    moved if

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ require
 
 document.forms["input-form"].onsubmit = () => {
     const firstName = document.getElementById("firstName").value
+    if (firstName === 'Mike') {
+        Rollbar.warning('Mike is almost not valid')
+    }
     const lastName = document.getElementById("lastName").value
     const email = document.getElementById("email").value
     const username = document.getElementById("username").value
@@ -11,7 +14,4 @@ Last Name: ${lastName}
 Email: ${email}
 Username: ${username}
 Password: ${password}`)
-}
-if (firstName === 'Mike') {
-    Rollbar.warning('Mike is almost not valid')
 }
